da49130e1ad68be13c7b152f0ca18546e59936e4
Alejandro Velasquez <VelasquezAlejandro044@gmail.com>
2021-10-05 08:28:47 -0500

    get up

--- sand_box.py
+++ sand_box.py
@@ -1,15 +1,1 @@
-fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
-
-def two_or_more_vowels():
-    fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
-    fruits_with_more_than_two_vowels = []
-    vowel_count = 0
-   
-    for fruit in fruits:
-        for letter in list(fruit.lower):
-            if letter in ('a', 'e', 'i', 'o', 'u'):
-                vowel_count += 1
-    return vowel_count
-    
-print(two_or_more_vowels)
-            
+1+1
