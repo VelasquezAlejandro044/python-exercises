ff350666ead1a63e6e864742be7120a077530f7d
Alejandro Velasquez <VelasquezAlejandro044@gmail.com>
2021-10-08 10:03:05 -0500

    up

--- delmeCodeUP.py
+++ delmeCodeUP.py
@@ -27,24 +27,36 @@ highest_balance = max([user["balance"] for user in d ])
 highest_balance_user = [x["name"] for x in d if x["balance"] == highest_balance]
 
 # 3h. Most common favorite fruit = strawberry = 9
+# def Most_common_favorite_fruit():
+#     fruits = list(set([x["favoriteFruit"] for x in d]))
+#     fav_fruits = [x["favoriteFruit"] for x in d]
+#     i=-1
+#     for fruit in fruits:
+#         i += 1
+#         print(f" {fruit} = {(fav_fruits.count(fruits[i]))}")
+# print(Most_common_favorite_fruit())        
+#print(Most_common_favorite_fruit())
+
 def Most_common_favorite_fruit():
     fruits = list(set([x["favoriteFruit"] for x in d]))
     fav_fruits = [x["favoriteFruit"] for x in d]
     i=-1
+    f = []
     for fruit in fruits:
         i += 1
-        print(f" {fruit} = {(fav_fruits.count(fruits[i]))}")
-        
-#print(Most_common_favorite_fruit())
+        f.append((fav_fruits.count(fruits[i]), fruit))
+    return max(f)
+
+print(Most_common_favorite_fruit())
+ 
 # 3i. Least most common favorite fruit
 
 # 3j. Total number of unread messages for all users = 210
-greetings = [user["greeting"] for user in d]
-numbers = sum([int(word) for str_ in greetings for word in str_.split() if word.isdigit()])
+# greetings = [user["greeting"] for user in d]
+# numbers = sum([int(word) for str_ in greetings for word in str_.split() if word.isdigit()])
 
 # for str_ in greetings:
 #     for word in str_.split():
 #         if word.isdigit():
 #             numbers.append(int(word))
         
-print(numbers)
